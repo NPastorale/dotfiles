8d3ef0ede4bfae1597e868c855815c3ca133de40
NPastorale <npastorale@gmail.com>
2020-03-11 18:22:14 +0100

    Refactored and added headphone detection

--- polybar/.config/polybar/bluetooth.sh
+++ polybar/.config/polybar/bluetooth.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+# Checks for bluetooth off
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
-    echo "%{F#66ffffff}"
+    echo "%{F#BF616A}"
+# Checks if connected and headphones
+elif [ $(bluetoothctl info | grep '00001108-0000-1000-8000-00805f9b34fb' | wc -c) -gt 0 ]; then
+    echo "%{F#2193ff}"
+# Checks for connected devices
+elif [ $(bluetoothctl info | grep 'Device' | wc -c) -eq 0 ]; then
+    echo ""
 else
-    if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then
-        echo ""
-    fi
     echo "%{F#2193ff}"
 fi
