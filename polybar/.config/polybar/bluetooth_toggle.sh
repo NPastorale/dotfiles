ad5017a71b5e09e574f0340df75b1faa5e29f68d
NPastorale <npastorale@gmail.com>
2020-03-11 18:21:15 +0100

    Added automatic connect to headphones

--- polybar/.config/polybar/bluetooth_toggle.sh
+++ polybar/.config/polybar/bluetooth_toggle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
-    bluetoothctl power on
+    bluetoothctl power on && bluetoothctl connect 04:52:C7:5B:70:1D &
 else
     bluetoothctl power off
 fi
