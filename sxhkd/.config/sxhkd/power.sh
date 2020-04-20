5eca231d2fa7c11f4c0cdb9093d53123a2ab77b6
NPastorale <npastorale@gmail.com>
2020-04-20 12:31:33 +0200

    Style change

--- sxhkd/.config/sxhkd/power.sh
+++ sxhkd/.config/sxhkd/power.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # rofi power menu
 
-chosen=$(echo -e "􀎠\n􀊅\n􀍿\n􀆨" | rofi -dmenu -i -lines 4 -theme power)
-[[ $chosen == *"Lock"* ]] && light-locker-command -l
-[[ $chosen == *"Suspend"* ]] && systemctl suspend
-[[ $chosen == *"Reboot"* ]] && systemctl reboot
-[[ $chosen == *"Shutdown"* ]] && systemctl poweroff
+chosen=$(echo -e "lock\nsuspend\nreboot\npoweroff" | rofi -dmenu -i -lines 4 -theme power)
+[[ $chosen == *"lock"* ]] && light-locker-command -l
+[[ $chosen == *"suspend"* ]] && systemctl suspend
+[[ $chosen == *"reboot"* ]] && systemctl reboot
+[[ $chosen == *"poweroff"* ]] && systemctl poweroff
