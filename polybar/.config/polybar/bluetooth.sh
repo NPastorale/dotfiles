9ac30fc7972605a164d3d796c7dfbe22432b0da6
NPastorale <npastorale@gmail.com>
2021-03-27 11:49:32 +0100

    Fixed headset detection when more than 1 device is connected

--- polybar/.config/polybar/bluetooth.sh
+++ polybar/.config/polybar/bluetooth.sh
@@ -3,7 +3,7 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
     echo "%{F#BF616A}"
 # Checks if connected and headphones
-elif [ $(bluetoothctl info | grep '00001108-0000-1000-8000-00805f9b34fb' | wc -c) -gt 0 ]; then
+elif [ $(bluetoothctl paired-devices | cut -d' ' -f2 | xargs -I[] -n1 bash -c "bluetoothctl info [] | grep -q 'Connected: yes' && bluetoothctl info [] | grep '00001108-0000-1000-8000-00805f9b34fb'" | wc -l) -gt 0 ]; then
     echo "%{F#2193ff}"
 # Checks for connected devices
 elif [ $(bluetoothctl info | grep 'Device' | wc -c) -eq 0 ]; then
